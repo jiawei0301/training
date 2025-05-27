f90b2a0c9539a6ee6aa6fb8aa58a8f273f0eec4a
jiaweizhu <jiaweizhu96@gmail.com>
2025-05-26 20:57:06 -0400

    MongoDB Crud

--- week2/MongoDBPractice/src/main/java/MongoDBCrud.java
+++ week2/MongoDBPractice/src/main/java/MongoDBCrud.java
@@ -4,23 +4,29 @@ import static com.mongodb.client.model.Filters.eq;
 
 public class MongoDBCrud {
     public static void main(String[] args) {
+        // Connect to MongoDB
         try (MongoClient client = MongoClients.create("mongodb://localhost:27017")) {
-            MongoDatabase db = client.getDatabase("mydb");
-            MongoCollection<Document> users = db.getCollection("users");
+            MongoDatabase database = client.getDatabase("mydb");
+            MongoCollection<Document> users = database.getCollection("users");
 
             // Create
             Document user = new Document("name", "Alice").append("age", 25);
             users.insertOne(user);
+            System.out.println("Inserted user");
 
             // Read
             Document result = users.find(eq("name", "Alice")).first();
-            System.out.println("Read: " + result.toJson());
+            if (result != null) {
+                System.out.println("Read user: " + result.toJson());
+            }
 
             // Update
             users.updateOne(eq("name", "Alice"), new Document("$set", new Document("age", 26)));
+            System.out.println("Updated age");
 
             // Delete
             users.deleteOne(eq("name", "Alice"));
+            System.out.println("Deleted user");
         }
     }
 }
