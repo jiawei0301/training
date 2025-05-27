d7126dad46cd56b0af7c9c0ba55fa65db3246105
jiaweizhu <jiaweizhu96@gmail.com>
2025-05-26 21:26:34 -0400

    fixed type

--- week2/CassandraPractice/src/main/java/CassandraCrud.java
+++ week2/CassandraPractice/src/main/java/CassandraCrud.java
@@ -9,7 +9,7 @@ public class CassandraCrud {
     public static void main(String[] args) {
         try (CqlSession session = CqlSession.builder()
                 .addContactPoint(new InetSocketAddress("127.0.0.1", 9042))
-                .withLocalDatacenter("datacenter1") // change if using a different DC
+                .withLocalDatacenter("datacenter1") 
                 .withKeyspace("mykeyspace")
                 .build()) {
 
